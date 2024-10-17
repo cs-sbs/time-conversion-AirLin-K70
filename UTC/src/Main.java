8c65371f68815fddf8f1764297e2f4840e7fc642
AirLin-K70 <2795374549@qq.com>
2024-10-17 16:01:05 +0800

    Add files via upload

--- UTC/src/Main.java
+++ UTC/src/Main.java
@@ -1,30 +1,15 @@
-package org.example;
-
-import java.util.Scanner;
-
+//TIP 要<b>运行</b>代码，请按 <shortcut actionId="Run"/> 或
+// 点击装订区域中的 <icon src="AllIcons.Actions.Execute"/> 图标。
 public class Main {
-    public static void main(String[] args){
-        int BT,UT,n,m;
-        Scanner in = new Scanner(System.in);
-        BT = in.nextInt();
-        if(BT<0 | BT>2359)
-        {
-            System.out.println("输入错误，请重新输入");
-        }
-        else
-        {
-            n = BT/100;
-            m = BT%100;
-            if(n<8)
-            {
-                n = n + 24;
-                UT = n*100+m;
-                System.out.println(UT - 800);
-            }
-            else
-            {
-                System.out.println(BT - 800);
-            }
+    public static void main(String[] args) {
+        //TIP 当文本光标位于高亮显示的文本处时按 <shortcut actionId="ShowIntentionActions"/>
+        // 查看 IntelliJ IDEA 建议如何修正。
+        System.out.printf("Hello and welcome!");
+
+        for (int i = 1; i <= 5; i++) {
+            //TIP 按 <shortcut actionId="Debug"/> 开始调试代码。我们已经设置了一个 <icon src="AllIcons.Debugger.Db_set_breakpoint"/> 断点
+            // 但您始终可以通过按 <shortcut actionId="ToggleLineBreakpoint"/> 添加更多断点。
+            System.out.println("i = " + i);
         }
     }
 }
